1c14965ab5abfddc74ce0e6abdbf5d5937538ac4
Seekerses <seekergodlike@gmail.com>
2023-05-30 17:17:08 +0300

    done

--- src/main/java/org/example/DCLSPlugin.java
+++ src/main/java/org/example/DCLSPlugin.java
@@ -1,58 +1,58 @@
 package org.example;
 
-/*
- * Copyright 2001-2005 The Apache Software Foundation.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-import org.apache.maven.model.Dependency;
+import org.apache.maven.artifact.Artifact;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
 import org.apache.maven.project.MavenProject;
 
-import java.util.stream.Collectors;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
-/**
- * Goal which touches a timestamp file.
- *
- * @goal touch
- * 
- * @phase process-sources
- */
-@Mojo(name = "config-create", defaultPhase = LifecyclePhase.GENERATE_RESOURCES)
-public class MyMojo
-    extends AbstractMojo
-{
+@Mojo(name = "config-create", defaultPhase = LifecyclePhase.PROCESS_RESOURCES, requiresDependencyCollection = ResolutionScope.COMPILE_PLUS_RUNTIME, requiresDependencyResolution = ResolutionScope.COMPILE_PLUS_RUNTIME)
+public class DCLSPlugin extends AbstractMojo {
+    @Parameter( defaultValue = "${project.build.directory}", property = "outputDirectory", required = true )
+    private File outputDirectory;
+
     @Parameter(defaultValue = "${project}", required = true, readonly = true)
     MavenProject project;
 
     public void execute() throws MojoExecutionException {
-        StringBuilder config = new StringBuilder();
-        config.append("sv=")
-                .append(project.getProperties().getProperty("maven.compiler.source"))
-                .append("tv=")
-                .append(project.getProperties().getProperty("maven.compiler.target"))
-                .append("src=")
-                .append(project.getCompileSourceRoots())
-                        .append("tmp=.")
-                                .append("lib=");
-        for(Dependency dependency: project.getDependencies()){
-            config.append(dependency.getSystemPath()).append(";");
+        String config = createConfigString();
+        File f = outputDirectory;
+        if ( !f.exists() )
+        {
+            f.mkdirs();
         }
-        System.out.println(config);
+        File prop = new File( f, "dcls.properties" );
+        try (FileWriter w = new FileWriter(prop)) {
+            w.write(config);
+        } catch (IOException e) {
+            throw new MojoExecutionException("Error creating file " + prop, e);
+        }
+    }
+
+    private String createConfigString(){
+        StringBuilder config = new StringBuilder();
+        config.append("sv=JAVA_")
+                .append(project.getProperties().getProperty("maven.compiler.source"))
+                .append(":")
+                .append("tv=JAVA_")
+                .append(project.getProperties().getProperty("maven.compiler.target"))
+                .append(":")
+                .append("src=")
+                .append(project.getCompileSourceRoots().toString().replace("[", "").replace("]", "")).append(":")
+                .append("tmp=.");
+        if (project.getArtifacts().size() != 0) {
+            config.append(":").append("lib=");
+            for (final Artifact artifact : project.getArtifacts()) {
+                config.append(artifact.getFile().getAbsolutePath()).append(";");
+            }
+        }
+        return config.toString();
     }
 }
